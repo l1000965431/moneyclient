4cc797254aa85ddd13f3423efceea8013cc7c582
l1000965431 <l1000965431@126.com>
2015-08-06 14:47:37 +0800

    修改订单model

--- src/main/java/com/money/config/MoneyServerMQ_Topic.java
+++ src/main/java/com/money/config/MoneyServerMQ_Topic.java
@@ -7,4 +7,13 @@ public class MoneyServerMQ_Topic {
 
     public static final String MONEYSERVERMQ_ORDERINSERT_TOPIC="MoneyServer_Order_Insert";
     public static final String MONEYSERVERMQ_ORDERINSERT_TAG="MoneyServer_Order_Insert";
+
+    public static final String MONEYSERVERMQ_ACTIVITYBUY_TOPIC="MoneyServer_Buy_Listener";
+    public static final String MONEYSERVERMQ_ACTIVITYBUY_TAG="MoneyServer_Buy_Listener";
+
+    public static final String MONEYSERVERMQ_LOTTERY_TOPIC="MoneyServer_Lottery_Listener";
+    public static final String MONEYSERVERMQ_LOTTERY_TAG="MoneyServer_Lottery_Listener";
+
+    public static final String MONEYSERVERMQ_SOMEFRAM_TOPIC="MoneyServer_SomeFarm_Listener";
+    public static final String MONEYSERVERMQ_SOMEFRAM_TAG="MoneyServer_SomeFarm_Listener";
 }
